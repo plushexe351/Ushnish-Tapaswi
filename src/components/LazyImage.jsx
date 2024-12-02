5127963bdf931b7a4642ab8e8f2a0f51d08db793
plushexe351 <geekyushnish@gmail.com>
2024-12-02 05:35:36 +0530

    Implemented LazyLoading for project images

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-const LazyImage = ({ src, alt, placeholderSrc, id, className }) => (
+const LazyImage = ({ src, alt, placeholderSrc }) => (
   <LazyLoadImage
-    src={src} // Actual image
-    alt={alt} // Alt text
-    effect="blur" // Lazy load effect (e.g., blur, opacity)
-    placeholderSrc={placeholderSrc} // Optional placeholder image
-    className={className}
+    src={src}
+    alt={alt}
+    effect="blur"
+    placeholderSrc={placeholderSrc}
   />
 );
 
