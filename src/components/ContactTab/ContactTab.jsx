8583f9cbf34c14a64b48248fe490f76d9a03b0db
plushexe351 <geekyushnish@gmail.com>
2025-04-18 21:48:36 +0530

    update ui : remove socials tab from ContactTab temporarily

--- src/components/ContactTab/ContactTab.jsx
+++ src/components/ContactTab/ContactTab.jsx
@@ -4,7 +4,7 @@ import ContactForm from "./ContactForm";
 
 const ContactTab = () => {
   const [view, setView] = useState("Message Me");
-  const navItems = ["Message Me", "Socials"];
+  const navItems = ["Message Me"];
   return (
     <div className="tab">
       <nav className="menubar">
